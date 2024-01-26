abefd5584d16137dddf4062e75fc0cd2be6e4f69
Hyeryeon-Seo <0168968060@naver.com>
2024-01-26 20:59:35 +0900

    feat(x): 헤더 새로고침기능 넣었다가 없앰

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 function Header() {
+  /*const refreshPage = () => {
+    window.location.reload(false);
+  }; */
+  // DOM제어문 그대로 window.location.reload하니까 작동안하고 위처럼 넣어주니까 됨(구글링)
+
   return (
     <header>
       {/*일반 div태그 -> h1태그로 변경*/}
-      <h1 className="headerTitle">My To-do List</h1>
+      <h1 className="headerTitle">
+        {/*onClick={refreshPage}새로고침기능 뺐다*/}
+        💕 My To-Do List 🐾
+      </h1>
       <h1 className="headerName">React 4기 서혜련</h1>
     </header>
   );
